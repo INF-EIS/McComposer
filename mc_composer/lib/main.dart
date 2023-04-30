08ad4a29452ad8d1eb14c67bc8977f87877817be
MatiesC-1952678 <maties.claesen@student.uhasselt.be>
2023-04-30 23:33:24 +0200

    made small change

--- mc_composer/lib/main.dart
+++ mc_composer/lib/main.dart
@@ -103,6 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    
     return Scaffold(
         appBar: AppBar(
           title: Row(children: <Widget>[
